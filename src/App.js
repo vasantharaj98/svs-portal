b2dc7531ca885dc7875db7416d857db917362318
vetrivel.r <vetrivel.r@rigelsoft.com>
2022-10-21 19:50:48 +0530

    chumma commit with file name changed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, {useState, useEffect, useContext} from "react";
-import Header from "./Layouts/Header/index.js"
+import React, { useState, useEffect, useContext } from "react";
+import Header from "./Layouts/Header/index.js";
 import Class from "./Pages/Class/index.js";
 import Bus from "./Pages/Bus/index.js";
 import Discount from "./Pages/Discount/index.js";
-import Student from "./Pages/Student/index.js"
-import Fees from "./Pages/Fees/index.js";
-import Year from "./Pages/Year/index"
+import Student from "./Pages/Student/Index.js";
+import Fees from "./Pages/Fees/Index.js";
+import Year from "./Pages/Year/index";
 import Login from "./Pages/Login/login";
 import Auth from "./Auth/Auth.js";
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import { getBusfees, loading, toast } from './actions/busfees';
-import { getYear } from './actions/year'
-import { useDispatch} from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { getBusfees, loading, toast } from "./actions/busfees";
+import { getYear } from "./actions/year";
+import { useDispatch } from "react-redux";
 import AuthContext from "./context/AuthProvider.js";
 import { getClass } from "./actions/class.js";
-import { getDiscount } from "./actions/discount"
+import { getDiscount } from "./actions/discount";
 import { getFees } from "./actions/fees.js";
 import { getStudent } from "./actions/student.js";
 
 function App() {
+  const { auth, setAuth } = useContext(AuthContext);
 
-  const {auth, setAuth} = useContext(AuthContext);
-  
   const [currentId, setCurrentid] = useState(null);
 
   const [vchange, setVchange] = useState(false);
@@ -30,36 +34,83 @@ function App() {
 
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    setAuth(JSON.parse(localStorage.getItem('login')))
-  },[])
+  useEffect(() => {
+    setAuth(JSON.parse(localStorage.getItem("login")));
+  }, []);
 
-  useEffect(()=>{
-        dispatch(getDiscount());
-        dispatch(getClass());
-        dispatch(getYear());
-        dispatch(getFees(year));
-        dispatch(getBusfees(year));
-        dispatch(getStudent(year));
-        dispatch(loading(false));
-        dispatch(toast(false));
-  },[dispatch, vchange, auth]);
+  useEffect(() => {
+    dispatch(getDiscount());
+    dispatch(getClass());
+    dispatch(getYear());
+    dispatch(getFees(year));
+    dispatch(getBusfees(year));
+    dispatch(getStudent(year));
+    dispatch(loading(false));
+    dispatch(toast(false));
+  }, [dispatch, vchange, auth]);
 
   return (
     <div className="App">
       <Router>
-      {auth?.data && <Header auth={auth} ></Header>}
-      <Routes>
-        <Route path='/login' element={auth?.data ? <Navigate to="/"/> : <Login/>}></Route>
-        <Route element={<Auth auth={auth} />}>
-        <Route path='/'element={<Student year={year} setYear={setYear} vchange={vchange} setVchange={setVchange} currentId={currentId} setCurrentid={setCurrentid}/>} exact></Route>
-        <Route path='/academicyear' element={<Year vchange={vchange} setVchange={setVchange}/>} ></Route>
-        <Route path='/busfees'  element={<Bus year={year} setYear={setYear} vchange={vchange} setVchange={setVchange} currentId={currentId} setCurrentid={setCurrentid}/>} ></Route>
-        <Route path='/class' element={<Class vchange={vchange} setVchange={setVchange}/>} ></Route>
-        <Route path='/discount' element={<Discount vchange={vchange} setVchange={setVchange}/>} ></Route>
-        <Route path='/academic_fees' element={<Fees year={year} setYear={setYear} vchange={vchange} setVchange={setVchange}/>} ></Route>
-        </Route>        
-      </Routes>
+        {auth?.data && <Header auth={auth}></Header>}
+        <Routes>
+          <Route
+            path="/login"
+            element={auth?.data ? <Navigate to="/" /> : <Login />}
+          ></Route>
+          <Route element={<Auth auth={auth} />}>
+            <Route
+              path="/"
+              element={
+                <Student
+                  year={year}
+                  setYear={setYear}
+                  vchange={vchange}
+                  setVchange={setVchange}
+                  currentId={currentId}
+                  setCurrentid={setCurrentid}
+                />
+              }
+              exact
+            ></Route>
+            <Route
+              path="/academicyear"
+              element={<Year vchange={vchange} setVchange={setVchange} />}
+            ></Route>
+            <Route
+              path="/busfees"
+              element={
+                <Bus
+                  year={year}
+                  setYear={setYear}
+                  vchange={vchange}
+                  setVchange={setVchange}
+                  currentId={currentId}
+                  setCurrentid={setCurrentid}
+                />
+              }
+            ></Route>
+            <Route
+              path="/class"
+              element={<Class vchange={vchange} setVchange={setVchange} />}
+            ></Route>
+            <Route
+              path="/discount"
+              element={<Discount vchange={vchange} setVchange={setVchange} />}
+            ></Route>
+            <Route
+              path="/academic_fees"
+              element={
+                <Fees
+                  year={year}
+                  setYear={setYear}
+                  vchange={vchange}
+                  setVchange={setVchange}
+                />
+              }
+            ></Route>
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
--- src/Pages/Student/Index.js
+++ src/Pages/Student/Index.js
@@ -1,84 +1,125 @@
-import React, {useEffect, useState} from 'react';
-import {styled} from '@mui/material/styles';
-import Table from '../../Components/Table/Table';
-import Adddata from './Addstudent';
-import Viewstudent from './ViewStudent';
-import { Box, Typography, Autocomplete, TextField } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "../../Components/Table/Table";
+import Adddata from "./Addstudent";
+import Viewstudent from "./viewStudent";
+import { Box, Typography, Autocomplete, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const columns = [
-    { id: 'studentId', label: 'Student Id', minWidth: 100 },
-    { id: 'name',align: 'center', label: 'Student Name', minWidth: 100 },
-    { id: 'mobileNumber',align: 'center', label: 'Mobile Number', minWidth: 100 },
-    { id: 'adharNumber',align: 'center', label: 'Aadhaar Number', minWidth: 100 },
-    { id: 'email',align: 'center', label: 'Email', minWidth: 100 },
-    { id: 'dob',align: 'center', label: 'DOB', minWidth: 100 },
-    { id: 'className',align: 'center', label: 'Class Name', minWidth: 100 },
-    { id: 'sectionName',align: 'center', label: 'Section Name', minWidth: 100 },
-    { id: 'action', label: 'Action', minWidth: 170, align: 'center', actiontype:[{view: true, edit: true, delete: true}]}
-  ];
-  
+  { id: "studentId", label: "Student Id", minWidth: 100 },
+  { id: "name", align: "center", label: "Student Name", minWidth: 100 },
+  {
+    id: "mobileNumber",
+    align: "center",
+    label: "Mobile Number",
+    minWidth: 100,
+  },
+  {
+    id: "adharNumber",
+    align: "center",
+    label: "Aadhaar Number",
+    minWidth: 100,
+  },
+  { id: "email", align: "center", label: "Email", minWidth: 100 },
+  { id: "dob", align: "center", label: "DOB", minWidth: 100 },
+  { id: "className", align: "center", label: "Class Name", minWidth: 100 },
+  { id: "sectionName", align: "center", label: "Section Name", minWidth: 100 },
+  {
+    id: "action",
+    label: "Action",
+    minWidth: 170,
+    align: "center",
+    actiontype: [{ view: true, edit: true, delete: true }],
+  },
+];
 
-  const Tablebox = styled('div')(({ theme }) => ({
-    marginTop : 80,
-    marginLeft : 80,
-    padding: theme.spacing(0, 3),
-  }));
+const Tablebox = styled("div")(({ theme }) => ({
+  marginTop: 80,
+  marginLeft: 80,
+  padding: theme.spacing(0, 3),
+}));
 
-const Bus = ({year, setYear, vchange, setVchange, currentId, setCurrentid}) => {
+const Bus = ({
+  year,
+  setYear,
+  vchange,
+  setVchange,
+  currentId,
+  setCurrentid,
+}) => {
+  const student = useSelector((state) => state.student);
 
-  const student = useSelector((state)=> state.student);
+  const [view, setView] = useState(false);
 
-  const[view, setView]=useState(false);
-
-  const studentView = student.data.map((v)=>{
-      return {...v, dob:`${v.dob[2]}-${v.dob[1]}-${v.dob[0]}`};
+  const studentView = student.data.map((v) => {
+    return { ...v, dob: `${v.dob[2]}-${v.dob[1]}-${v.dob[0]}` };
   });
 
-  const batchYear = useSelector((state)=> state.year);
+  const batchYear = useSelector((state) => state.year);
 
-  const top100Films = batchYear?.data.map((ye)=>{
+  const top100Films = batchYear?.data.map((ye) => {
     return ye.batchYear;
- });
-  
-  useEffect( () => {
-    if(student.successMessage){
-      toast(student.Message,
-        {position: toast.POSITION.BOTTOM_RIGHT});
+  });
+
+  useEffect(() => {
+    if (student.successMessage) {
+      toast(student.Message, { position: toast.POSITION.BOTTOM_RIGHT });
     }
-  },[student]);
+  }, [student]);
 
   return (
     <>
-    <Tablebox>
-      {view ?
-      <Viewstudent setView={setView} setCurrentid={setCurrentid} currentId={currentId} year={year}></Viewstudent>
-      :
-      <div>
-      <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px', flexWrap: 'wrap'}}>
-        <Typography variant="h5" sx={{ fontWeight: '600'}} >Students</Typography>
-        <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={top100Films}
-        sx={{ width: 300 }}
-        value={top100Films ? year : null}
-        onChange={(event, value) => {
-          setYear(value) ;
-          setVchange(!vchange);
-        }}
-        renderInput={(params) => <TextField {...params} label="Select Year" />}
-        />
-        <Adddata year={year} button="Add Student"></Adddata>
-    </Box>
-    <Table setCurrentid={setCurrentid} columns={columns} rows={studentView} setView={setView}/>
-      </div>
-}
-    </Tablebox>
+      <Tablebox>
+        {view ? (
+          <Viewstudent
+            setView={setView}
+            setCurrentid={setCurrentid}
+            currentId={currentId}
+            year={year}
+          ></Viewstudent>
+        ) : (
+          <div>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: "20px",
+                flexWrap: "wrap",
+              }}
+            >
+              <Typography variant="h5" sx={{ fontWeight: "600" }}>
+                Students
+              </Typography>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: 300 }}
+                value={top100Films ? year : null}
+                onChange={(event, value) => {
+                  setYear(value);
+                  setVchange(!vchange);
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Select Year" />
+                )}
+              />
+              <Adddata year={year} button="Add Student"></Adddata>
+            </Box>
+            <Table
+              setCurrentid={setCurrentid}
+              columns={columns}
+              rows={studentView}
+              setView={setView}
+            />
+          </div>
+        )}
+      </Tablebox>
     </>
-
-  )
-}
+  );
+};
 
 export default Bus;
